4d6e606ed2c48196fb9dd95a937c97397e68572d
Iván Parra <114946173+IvanParra12@users.noreply.github.com>
2022-11-07 20:07:28 +0100

    Create Examen01.java

---
+++ 01_ExamenTema1/Examen01.java
@@ -0,0 +1,35 @@
+package examen01;
+
+/**
+ *
+ * @author Iván Darío Parra
+ */
+public class Examen01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte var1 = 1;
+        short var2 = 2;
+        int var3 = 3;
+        long var4 = 4;
+        float var5 = 5;
+        double var6 = 5.5;
+        char var7 = 6;
+        boolean var8 = true;
+        
+        System.out.println("El contenido de var1 es: " +var1);
+        System.out.println("El contenido de var2 es: " +var2);
+        System.out.println("El contenido de var3 es: " +var3);
+        System.out.println("El contenido de var4 es: " +var4);
+        System.out.println("El contenido de var5 es: " +var5);
+        System.out.println("El contenido de var6 es: " +var6);
+        System.out.println("El contenido de var7 es: " +var7);
+        System.out.println("El contenido de var8 es: " +var8);
+
+        
+    }
+    
+}
