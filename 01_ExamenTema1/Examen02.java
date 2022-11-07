6074ee8ecd225e0e4d08e92ea9993e1223814a1e
Iván Parra <114946173+IvanParra12@users.noreply.github.com>
2022-11-07 20:08:01 +0100

    Create Examen02.java

---
+++ 01_ExamenTema1/Examen02.java
@@ -0,0 +1,50 @@
+package examen02;
+import java.util.*;
+/**
+ *
+ * @author usuario-mañana
+ */
+public class Examen02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("====================");
+        System.out.println("=    Iván Darío    =");
+        System.out.println("=   Parra Iglesias =");
+        System.out.println("=                  =");
+        System.out.println("====================");
+        
+        System.out.println("\n");
+        
+        System.out.println("Introduce el valor de la variable:");
+        int var1 = new Scanner(System.in).nextInt();
+        
+        System.out.println("Introduce el valor de la variable:");
+        double var2 = new Scanner(System.in).nextDouble();
+        
+        System.out.println("Introduce el valor de la variable:");
+        long var3 = new Scanner(System.in).nextLong();
+
+        System.out.println("Introduce el valor de la variable:");
+        byte var4 = new Scanner(System.in).nextByte();
+
+        System.out.println("Introduce el valor de la variable:");
+        short var5 = new Scanner(System.in).nextShort();
+        
+        
+        System.out.println("El valor de la variable1 es: " +var1);
+        System.out.println("El valor de la variable2 es: " +var2);
+        System.out.println("El valor de la variable3 es: " +var3);
+        System.out.println("El valor de la variable4 es: " +var4);
+        System.out.println("El valor de la variable5 es: " +var5);
+
+
+
+
+
+    }
+    
+}
