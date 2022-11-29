58afd7131c5c0ec66c24674521608a61d44e6060
Iván Parra <114946173+IvanParra12@users.noreply.github.com>
2022-11-29 17:00:37 +0100

    Add files via upload

---
+++ 02_EjerciciosT2/Ejercicio42.java
@@ -0,0 +1,19 @@
+package ejercicio42;
+
+/**
+ *
+ * @author Iván Parra <iparigl784@g.educaand.es>
+ */
+public class Ejercicio42 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int num = (int)(Math.random()*(65-29+1)+29);
+       
+       System.out.println("Numero aleatorio entre 29 y 65: "
+               +num);
+    }
+    
+}
