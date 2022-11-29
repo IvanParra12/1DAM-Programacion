3f8e31fcc7a85ec362038778d779285d4124e129
Iván Parra <114946173+IvanParra12@users.noreply.github.com>
2022-11-29 03:17:10 +0100

    Add files via upload

---
+++ 02_EjerciciosT2/Ejercicio30.java
@@ -0,0 +1,20 @@
+package ejercicio30;
+
+/**
+ *
+ * @author Iván Parra <iparigl784@g.educaand.es>
+ */
+public class Ejercicio30 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //a) Es una CheckedException
+        //b) Es una RuntimeException     
+        //c) Es una RuntimeException      
+
+
+    }
+
+}
