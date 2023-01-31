ab87566473daca7fc4b422408a7cdbbe04945ebf
Iván Parra <114946173+IvanParra12@users.noreply.github.com>
2023-01-31 17:36:07 +0100

    Add files via upload

---
+++ 05_EjerciciosT4/Prácticas/Ejercicio03.java
@@ -0,0 +1,21 @@
+package ejercicio03;
+import daw.partido.MarcadorBaloncesto;
+import java.time.*;
+/**
+ *
+ * @author Iván Parra <iparigl784@g.educaand.es>
+ */
+public class Ejercicio03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        MarcadorBaloncesto m1 = new MarcadorBaloncesto("Pepe", "Manolo");
+        
+        System.out.println(m1.toString());
+        
+    }
+
+}
