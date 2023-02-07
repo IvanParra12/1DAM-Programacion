6b16e1af56fcdb6318412e56c7ff0ab90d7d1a0a
Iván Parra <114946173+IvanParra12@users.noreply.github.com>
2023-02-07 18:29:41 +0100

    Add files via upload

---
+++ 05_EjerciciosT4/Prácticas/CuentaCorriente.java
@@ -0,0 +1,37 @@
+package daw.persona;
+import daw.persona.*;
+import java.math.*;
+/**
+ *
+ * @author Iván Parra <iparigl784@g.educaand.es>
+ */
+public class CuentaCorriente {
+    //ATRIBUTOS
+    private int numero;
+    private double saldo;
+    
+    //CONSTRUCTORES
+    public CuentaCorriente() {
+        numero = (int)Math.floor(Math.random()*1000);
+        saldo = 0;
+    }
+    
+    public CuentaCorriente(int n) {
+        numero = n;
+        saldo = 0;
+    }
+    
+    public CuentaCorriente(int n, double s) {
+        numero = n;
+        saldo = s;
+    }
+    
+    //SETTERS
+    public void sumarDinero(int cantidad) {
+        saldo += cantidad;
+    }
+    
+    public void retirarDinero(int cantidad) {
+        saldo -= cantidad;
+    }
+}
