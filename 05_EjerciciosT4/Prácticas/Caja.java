e91f4e288307013e9ec5b2967bfed3b3c16d016e
Iván Parra <114946173+IvanParra12@users.noreply.github.com>
2023-02-07 19:08:33 +0100

    Add files via upload

---
+++ 05_EjerciciosT4/Prácticas/Caja.java
@@ -0,0 +1,51 @@
+package daw.objetos;
+
+/**
+ *
+ * @author Iván Parra <iparigl784@g.educaand.es>
+ */
+public class Caja {
+    //ATRIBUTOS
+    private boolean abierto;
+    private String mensaje;
+    
+    //CONSTRUCTORES
+    public Caja() {
+        abierto = false;
+        mensaje = "Viva el tema 7";
+    };
+    
+    public Caja(String m) {
+        mensaje = m;
+        abierto = false;
+    }
+    
+    public Caja(boolean a, String m) {
+        abierto = a;
+        mensaje = m;
+    }
+    //GETTERS
+    public String getMensaje() {
+        return mensaje;
+    }
+    
+    public boolean getEstado() {
+        if(abierto) {
+            return true;
+        } else{
+           return false;
+        }
+    }
+    
+    //SETTERS
+    public void setMensaje(String m) {
+        if(abierto) {
+            mensaje = m;
+        }
+    }
+    
+    public void pasarMayusculas() {
+        mensaje = mensaje.toUpperCase();
+    }
+    
+}
