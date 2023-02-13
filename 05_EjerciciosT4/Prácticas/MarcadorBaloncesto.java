bebaeb715710aca57bf3e0c2bac3c377e77b118e
Iván Parra <114946173+IvanParra12@users.noreply.github.com>
2023-02-13 20:47:51 +0100

    Add files via upload

---
+++ 05_EjerciciosT4/Prácticas/MarcadorBaloncesto.java
@@ -0,0 +1,75 @@
+package daw.partido;
+import java.time.*;
+
+/**
+ *
+ * @author Iván Parra <iparigl784@g.educaand.es>
+ */
+public class MarcadorBaloncesto {
+    //ATRIBUTOS
+    private String nombreLocal;
+    private String nombreVisitante;
+    private int puntosLocal;
+    private int puntosVisitante;
+    private LocalDate fecha;
+    
+    //CONSTRUCTORES
+    public MarcadorBaloncesto(String nL, String nV) {
+        nombreLocal = nL;
+        nombreVisitante = nV;
+        puntosLocal = 0;
+        puntosVisitante = 0;
+        fecha = LocalDate.now();
+    }
+    
+    public MarcadorBaloncesto(String nL, String nV, LocalDate f)  {
+        nombreLocal = nL;
+        nombreVisitante = nV;
+        fecha = f;
+        puntosLocal = 0;
+        puntosVisitante = 0;
+    }
+    
+    public MarcadorBaloncesto(String nL, int pL, 
+            String nV, int pV, LocalDate f) {
+        nombreLocal = nL;
+        nombreVisitante = nV;
+        fecha = f;
+        puntosLocal = pL;
+        puntosVisitante = pV;
+    }
+    
+    //GETTERS
+    public LocalDate getFecha() {
+        return fecha;
+    }
+    
+    @Override
+    
+    public String toString() {
+        String aux = "Hay un partido entre " +nombreLocal+  " y "
+                +nombreVisitante+ " ,ambos equipos tienen: " 
+                +puntosLocal+ " y " +puntosVisitante+ " puntos respectivamente"
+                + " \nY se juega en la fecha: " +fecha;
+        return aux;
+    }
+    
+    //SETTERS
+    public void anadirCanasta(char equipo, int puntos) {
+       
+        if(equipo == 'L') {
+            
+            this.puntosVisitante += puntos;
+        }
+        else if(equipo == 'V') {
+            
+            this.puntosLocal += puntos;
+        }
+    }
+    
+    public void reset() {
+        
+        this.puntosLocal = 0;
+        this.puntosVisitante = 0;
+    }
+}
