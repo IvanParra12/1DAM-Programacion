abfe0c6e80db5e42aaa3098754921c58423c1501
Iván Parra <114946173+IvanParra12@users.noreply.github.com>
2023-02-13 21:33:43 +0100

    Add files via upload

---
+++ 05_EjerciciosT4/Prácticas/Oficina.java
@@ -0,0 +1,103 @@
+package daw.persona;
+import daw.persona.*;
+import java.util.*;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+/**
+ *
+ * @author Iván Parra <iparigl784@g.educaand.es>
+ */
+public class Oficina {
+
+    //ATRIBUTOS
+    private String nombre;
+    private ArrayList<Persona> trabajadores;
+
+    //CONSTRUCTORES
+    public Oficina() {
+        nombre = "Industrias DAW";
+        
+        trabajadores = new ArrayList<Persona>();
+        trabajadores.add(new Persona("Ana Diaz Diaz",
+                33333333, 'P', 1200,
+                LocalDate.parse("21/05/1985",
+                        DateTimeFormatter.ofPattern("dd/MM/yyyy"))));
+        trabajadores.add(new Persona("Luis Lopez Lopez",
+                22222222, 'J', 1000,
+                LocalDate.parse("10/09/1995",
+                        DateTimeFormatter.ofPattern("dd/MM/yyyy"))));
+        trabajadores.add(new Persona("Antonio Perez Perez",
+                11111111, 'H', 900,
+                LocalDate.parse("28/02/2000",
+                        DateTimeFormatter.ofPattern("dd/MM/yyyy"))));
+    }
+
+    public Oficina(String n) {
+        nombre = n;
+        trabajadores = null;
+    }
+
+    public Oficina(String n, int tipo) {
+        nombre = n;
+
+        if (tipo <= 0 || tipo > 3) {
+            trabajadores = new ArrayList<Persona>();
+        } else if (tipo == 1) {
+            trabajadores.add(new Persona("Antonio Perez Perez",
+                    11111111, 'H', 900,
+                    LocalDate.parse("28/02/2000",
+                            DateTimeFormatter.ofPattern("dd/MM/yyyy"))));
+        } else if (tipo == 2) {
+            trabajadores.add(new Persona("Luis Lopez Lopez",
+                    22222222, 'J', 1000,
+                    LocalDate.parse("10/09/1995",
+                            DateTimeFormatter.ofPattern("dd/MM/yyyy"))));
+            trabajadores.add(new Persona("Antonio Perez Perez",
+                    11111111, 'H', 900,
+                    LocalDate.parse("28/02/2000",
+                            DateTimeFormatter.ofPattern("dd/MM/yyyy"))));
+        } else if (tipo == 3) {
+            trabajadores.add(new Persona("Ana Diaz Diaz",
+                    33333333, 'P', 1200,
+                    LocalDate.parse("21/05/1985",
+                            DateTimeFormatter.ofPattern("dd/MM/yyyy"))));
+            trabajadores.add(new Persona("Luis Lopez Lopez",
+                    22222222, 'J', 1000,
+                    LocalDate.parse("10/09/1995",
+                            DateTimeFormatter.ofPattern("dd/MM/yyyy"))));
+            trabajadores.add(new Persona("Antonio Perez Perez",
+                    11111111, 'H', 900,
+                    LocalDate.parse("28/02/2000",
+                            DateTimeFormatter.ofPattern("dd/MM/yyyy"))));
+        }
+    }
+
+    //GETTERS
+    public ArrayList<Persona> getTrabajadores() {
+        return this.trabajadores;
+    }
+    @Override
+    public String toString() {
+        String aux = "Nombre: " + this.nombre + "\n";
+        
+        for(Persona p : trabajadores) {
+            aux += p.toString() + "\n";
+        }
+        return aux;
+    }
+    
+    //SETTERS
+    public void sumarEmpleado(Persona p) {
+        
+        this.trabajadores.add(p);
+    }
+    
+    public void sumarEmpleado(String nombre, String DNI, double sueldo,
+                                LocalDate fechaNac) {
+        DNI dni = new DNI(DNI);
+        Persona p = new Persona(nombre, dni, sueldo, fechaNac);
+        
+        this.trabajadores.add(p);
+    }
+}
