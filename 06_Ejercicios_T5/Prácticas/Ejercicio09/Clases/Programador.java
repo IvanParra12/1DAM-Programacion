0d6b61bbf6e1106d52af21174f1104e100329403
Iván Parra <114946173+IvanParra12@users.noreply.github.com>
2023-03-13 21:08:49 +0100

    Add files via upload

--- 06_Ejercicios_T5/Prácticas/Ejercicio09/Clases/Programador.java
+++ 06_Ejercicios_T5/Prácticas/Ejercicio09/Clases/Programador.java
@@ -11,17 +11,43 @@ public class Programador extends TrabajadorTecleante{
     private Map <String, String> lenguajes = new HashMap<>();
     
     //CONSTRUCTOR
+
+    /**
+     *
+     * @param n Nombre del Programador
+     * @param s Sueldo del Programador
+     * @param ndni Numero del DNI del Programador
+     * @param ldni Letra del DNI del Programador
+     * @param ppm Pulsaciones por minuto del Programador
+     */
     public Programador(String n, int s, int ndni, char ldni, int ppm){
         super(n, s, ndni, ldni, ppm);
     }
     
     //SETTERS
+
+    /**
+     *
+     * @param lenguaje Introduce el nombre del lenguaje que se va a aprender
+     * @param holaMundo Introduce como se programa la frase Hola Mundo en
+     * ese lenguaje
+     */
     public void aprenderLenguaje(String lenguaje, String holaMundo){
         
         this.lenguajes.put(lenguaje, holaMundo);
     }
     
     //GETTERS
+
+    /**
+     *
+     * @param lenguaje El lenguaje que se va a comprobar
+     * @return Devuelve como se programa la frase Hola Mundo con el 
+     * lenguaje pasado como parámetro
+     * @throws IllegalStateException Lanza una excepción 
+     * en caso de que el lenguaje introducido no esté en el Map
+     * 
+     */
     public String programarHolaMundo(String lenguaje) 
             throws IllegalStateException {
         if(!this.lenguajes.containsKey(lenguaje)) {
